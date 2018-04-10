ff8463732aec669469ae59cd4a15d3723ddb0d90
SilentOne <SilentOne@onet.pl>
2018-04-10 21:37:32 +0200

    Testing MediaPlayer

--- app/src/main/java/com/example/android/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/android/mediaplayer/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.mediaplayer;
 
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.test);
+        final Button play = findViewById(R.id.play);
+        Button pause = findViewById(R.id.pause);
+
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mediaPlayer.start();
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mediaPlayer.pause();
+            }
+        });
     }
 }
